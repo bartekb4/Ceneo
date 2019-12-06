98817201ab80f1e86709ebce81ae9f6593eda5df
ddoox <dudek746@gmail.com>
2019-12-06 19:09:55 +0100

    dynamic input + some exceptions

--- src/com/company/SetItem.java
+++ src/com/company/SetItem.java
@@ -5,6 +5,8 @@ import java.util.Scanner;
 public class SetItem {
 
     private Item swap = new Item();
+    private double min_price = 0;
+    private double number_double = -99.99;
 
     public Item Set(boolean first){
 
@@ -23,8 +25,6 @@ public class SetItem {
         this.swap.setName(input);
 
 
-        double number_double = -99.99;
-
 
         System.out.println("Podaj minimalną reputację sprzedającego:");
         do{
@@ -40,6 +40,7 @@ public class SetItem {
 
 
 
+        reset();
         System.out.println("Podaj minimalną cenę produktu:");
         do{
             try {
@@ -50,25 +51,38 @@ public class SetItem {
                 System.out.println("Podaj liczbę większą lub równą 0:");
             }
         }while(number_double < 0);
+        this.min_price = number_double;
         this.swap.setMin_price(number_double);
 
 
 
+        reset();
         System.out.println("Podaj maksymalną cenę produktu [0 - brak ceny maksymalnej]:");
         do{
             try {
                 input = user_text.nextLine();
                 number_double = Double.valueOf(input);
+
+                if(number_double < this.min_price && number_double != 0){
+                    System.out.println("Trzeba było wybrać cenę maksymalną większą niż minimalna -_-");
+                    this.swap.setMax_price(0);
+                }
+                else{
+                    this.swap.setMax_price(number_double);
+                }
+
             }
             catch (NumberFormatException e) {
                 System.out.println("Podaj liczbę większą lub równą 0:");
             }
         }while(number_double < 0);
-        this.swap.setMax_price(number_double);
 
         return this.swap;
     }
 
 
+    private void reset(){
+        this.number_double = -99.99;
+    }
 
 }

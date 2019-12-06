ab60ce2a4643ec5ff0c004a8efebb757fae9de30
ddoox <dudek746@gmail.com>
2019-12-06 23:03:44 +0100

    timestamps

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,7 @@ package com.company;
 import org.jsoup.HttpStatusException;
 
 import java.io.IOException;
+import java.sql.Timestamp;
 import java.text.ParseException;
 import java.util.Scanner;
 
@@ -20,13 +21,17 @@ public class Main {
         DataProcessor dp2 = new DataProcessor();
         DataProcessor dp3 = new DataProcessor();
 
+        System.out.println("\n" + new Timestamp(System.currentTimeMillis()) + "\n");
+
         Scanner user_text = new Scanner(System.in);
 
+        long time_start = 0;
         int ile_przedmiotow = 404; //ile przedmiotow wyszukujemy
 
         //Ilosc produktow do porownania
         System.out.println("Ile różnych przedmiotów chcesz wyszukać? [1-3]:");
         String input = "";
+
         do{
             try {
                 input = user_text.nextLine();
@@ -37,12 +42,12 @@ public class Main {
             }
         }while(ile_przedmiotow > 4 || ile_przedmiotow <0);
 
+
         switch(ile_przedmiotow){
             case 0:
 
                 System.out.println("Dziękujemy za skorzystanie z naszej wyszukiwarki. Może następnym razem uda nam się jakoś pomóc");
                 return;
-
 
             case 1:
 
@@ -50,13 +55,19 @@ public class Main {
 
                 cc.setItem(Creator.Set(true));
 
+                System.out.println("\n" + new Timestamp(System.currentTimeMillis()) + "\n");
+                System.out.println("Proszę czekać...\n\n");
+                time_start = System.currentTimeMillis();
+
                 try {
                     dp.find_best_deal_for_id(dp.request_product_soup(cc));
                 } catch (HttpStatusException e) {
-                    searchException.userError("Co robisz bandyto","Halko");
+                    searchException.userError("Co ty tutaj robisz?","Jak?");
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
+
+                System.out.println("\n" + "Czas zapytania:" +Float.valueOf((System.currentTimeMillis() - time_start)/1000) + "sek" +"\n");
 
                 break;
 
@@ -68,13 +79,19 @@ public class Main {
                 cc.setItem(Creator21.Set(true));
                 cc2.setItem(Creator22.Set(false));
 
+                System.out.println("\n" + new Timestamp(System.currentTimeMillis()) + "\n");
+                System.out.println("Proszę czekać...\n\n");
+                time_start = System.currentTimeMillis();
+
                 try {
                     dp.find_best_deal_for_id(dp.request_product_soup(cc),dp2.request_product_soup(cc2));
                 } catch (HttpStatusException e) {
-                    searchException.userError("Co robisz bandyto","Halko");
+                    searchException.userError("Co ty tutaj robisz?","Jak?");
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
+
+                System.out.println("\n" + "Czas zapytania:" +Float.valueOf((System.currentTimeMillis() - time_start)/1000) + "sek" +"\n");
 
                 break;
 
@@ -89,13 +106,19 @@ public class Main {
                 cc2.setItem(Creator32.Set(false));
                 cc3.setItem(Creator33.Set(false));
 
+                System.out.println("\n" + new Timestamp(System.currentTimeMillis()) + "\n");
+                System.out.println("Proszę czekać...\n\n");
+                time_start = System.currentTimeMillis();
+
                 try {
                     dp.find_best_deal_for_id(dp.request_product_soup(cc),dp2.request_product_soup(cc2),dp3.request_product_soup(cc3));
                 } catch (HttpStatusException e) {
-                    searchException.userError("Co robisz bandyto","Halko");
+                    searchException.userError("Co ty tutaj robisz?","Jak?");
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
+
+                System.out.println("\n" + "Czas zapytania:" +Float.valueOf((System.currentTimeMillis() - time_start)/1000) + "sek" +"\n");
 
                 break;
         }
